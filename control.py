aa52c35160e55dc9287738967ec98004135f3bdb
Alvin <alvinhuang9@gmail.com>
2024-02-05 13:21:37 +0000

    changing setpoint

--- control.py
+++ control.py
@@ -4,8 +4,8 @@ class ctl:
         self.dKp, self.dKi, self.dKd = distance_coeffs
         self.aKp, self.aKi, self.aKd = angle_coeffs
 
-        self.distance_ctl = PID(self.dKp, self.dKi, self.dKd, 100)
-        self.angle_ctl = PID(self.aKp, self.aKi, self.aKd, 100)
+        self.distance_ctl = PID(self.dKp, self.dKi, self.dKd, setpoint=100)
+        self.angle_ctl = PID(self.aKp, self.aKi, self.aKd, setpoint=320)
 
     def compute_speeds(self, distance, x_coord):
         straight_line_speed = self.distance_ctl(distance)
