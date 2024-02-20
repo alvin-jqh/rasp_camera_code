6d4d8fcab0f721b9fabecf3267cfab08cbe4cbd5
Alvin <alvinhuang9@gmail.com>
2024-02-20 14:35:42 +0000

    following code works, probabaly will require tuning

--- following.py
+++ following.py
@@ -2,20 +2,38 @@ import cv2
 import numpy as np
 
 from read_matrices import read_intrinsics, read_R_T
-from camera_class import Camera, rectify
+from camera_class import Camera, rectify, undistort
 
-from instance_tracking_class import instance_segmentor
+from box_tracking import tracker
+from live_classes import object_detector
+from live_classes import gesture_recogniser
 
-from point_depth import calculate_depth
+from feature_distance import match_distance
 
 from comms import SerialCommunication
 from control import ctl
+
+def draw_IDs(objects, frame):
+    for objectID, bounding_boxes in objects.items():
+        x,y,w,h = bounding_boxes
+        cX = int(x + w/2)
+        cY = int(y + h/2)
+
+        text = f"ID {objectID}"
+
+        # draw their bounding box centroids and print their ID
+        cv2.putText(frame, text, (cX - 10, cY - 10),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 165, 255), 2)
+        cv2.circle(frame, (cX, cY), 4, (0, 165, 255), -1 )
+
+    return frame
 
 def main(cameraL_id:int, cameraR_id:int, width: int, height: int, 
          port:str, baudrate:int = 9600, timeout:int = 1):
     
     object_model_path = "models\efficientdet_lite0.tflite"
-    segmentor_model_path = "models\selfie_segmenter.tflite"
+    gesture_model_path = "models\gesture_recognizer.task"
+    template_path = "images\cat_image_2.jpg"
 
     # get all the parameters for both cameras
     camera_matrix_L, distortion_L = read_intrinsics("camera_parameters\camera0_intrinsics.dat")
@@ -31,74 +49,135 @@ def main(cameraL_id:int, cameraR_id:int, width: int, height: int,
     baseline = T2[0]
 
     # intialise both cameras
-    left = Camera(camera_matrix= camera_matrix_L, distortion_coefficients= distortion_L,
-                    camera_id=cameraL_id, width=width, height=height)
+    left_cam = Camera(camera_matrix= camera_matrix_L, distortion_coefficients= distortion_L,
+                      camera_id=cameraL_id, width=width, height=height)
 
-    right = Camera(camera_matrix= camera_matrix_R, distortion_coefficients= distortion_R,
-                    camera_id=cameraR_id, width=width, height=height)
+    right_cam = Camera(camera_matrix= camera_matrix_R, distortion_coefficients= distortion_R,
+                       camera_id=cameraR_id, width=width, height=height)
+    
+    mapxL, mapyL, mapxR, mapyR, image_ROI = rectify(height, width, left_cam.camera_matrix, left_cam.dist, 
+                                                    right_cam.camera_matrix, right_cam.dist, R2, T2)
+    _, _, new_width, new_height = image_ROI
 
-    left_inst = instance_segmentor(object_model_path,segmentor_model_path)
-    right_inst = instance_segmentor(object_model_path,segmentor_model_path)
+    # object detection
+    left_detect = object_detector(object_model_path)
+    right_detect = object_detector(object_model_path)
 
-    # intialise the centroids
-    L_target_centroid = None
-    R_target_centroid = None
+    # box tracking
+    left_tracker = tracker(template_path)
+    right_tracker = tracker(template_path)
 
-    # initialise comms
+    # gesture recognition
+    gr = gesture_recogniser(gesture_model_path)
+
+    # initialise the distance matching and variables
+    dc = match_distance()
+    distance = 100    
+    x_coord = int(width/2)
+    matched_image = None
+
+    # false means stop, true means go
+    move_state = False
+
     line = SerialCommunication(port, baudrate, timeout)
-
-    # intialise controller
-    controller = ctl((1, 0, 0), (1, 0, 0))
-
-    new_L_speed = 0
-    new_R_speed = 0
+    line.open_connection()
+    controller = ctl(-2, -0.2, x_coord)
     proximity_flag = False
 
-    target_x = width / 2 # initalise the centre of the target
-
-    while left.opened() and right.opened:
+    while left_cam.opened() and right_cam.opened:
         measured_L_speed, measured_R_speed, proximity_flag = line.read_speeds()
 
-        left_image = left.read_frame()
-        right_image = right.read_frame()
+        distorted_left = left_cam.read_frame()
+        distorted_right = right_cam.read_frame()
 
-        # rectify and undistort the image based on the matrices produced from calibration
-        undistorted_left, undistorted_right = rectify(left_image.copy(), right_image.copy(), 
-                                                      left.camera_matrix, left.dist, 
-                                                      right.camera_matrix, right.dist, R2, T2)
+        undistorted_left = undistort(distorted_left, mapxL, mapyL, image_ROI)
+        undistorted_right = undistort(distorted_right, mapxR, mapyR, image_ROI)
 
-        left_detected_frame = left_inst.loop_function(undistorted_left)
-        right_detected_frame = right_inst.loop_function(undistorted_right)
+        left_bboxes = left_detect.loop_function(undistorted_left)
+        left_annotated_frame = left_detect.get_annotated_image()
 
-        L_target_centroid = left_inst.get_traget_centroid()
-        R_target_centroid = right_inst.get_traget_centroid()
+        right_bboxes = right_detect.loop_function(undistorted_right)
+        right_annotated_frame = right_detect.get_annotated_image()
 
-        if L_target_centroid is not None and R_target_centroid is not None:
-            depth_cm = calculate_depth(L_target_centroid, R_target_centroid, avg_focal_length, baseline)
-            cv2.putText(left_detected_frame, f"{depth_cm} cm", (L_target_centroid[0] + 20, L_target_centroid[1] + 60),
-                            cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+        left_objects, left_corners = left_tracker.update(left_bboxes, undistorted_left)
+        right_objects, right_corners = right_tracker.update(right_bboxes, undistorted_right)
+
+        left_target_ID = left_tracker.get_target_ID()
+        right_target_ID = right_tracker.get_target_ID()
+
+        if left_objects:
+            left_annotated_frame = draw_IDs(left_objects, left_annotated_frame)
+
+        if left_target_ID is not None:
+            left_target_bbox = left_tracker.get_target_bbox()
+            x, y, w, h = left_target_bbox
+            cX = int(x + w/2)
+            cY = int(y + h/2)
+            cv2.putText(left_annotated_frame, "Target", (cX - 30, cY + 20),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
             
-            cv2.putText(right_detected_frame, f"{depth_cm} cm", (R_target_centroid[0] + 20, R_target_centroid[1] + 60),
-                            cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-            
-            target_x = L_target_centroid[0] # set the x coordinate of the target
+            target_crop = undistorted_left[y:y+h, x:x+w]
+            recognition_frame, gestures = gr.loop_function(target_crop)
 
-        new_L_speed, new_R_speed = controller.compute_speeds(depth_cm, target_x)
+            if gestures:
+                if gestures == "Thumb_Up":
+                    move_state = True
+                elif gestures == "Thumb_Down":
+                    move_state = False
+
+        if right_objects:
+            right_annotated_frame = draw_IDs(right_objects, right_annotated_frame)
+
+        if right_target_ID is not None:
+            right_target_bbox = right_tracker.get_target_bbox()
+            x, y, w, h = right_target_bbox
+            cX = int(x + w/2)
+            cY = int(y + h/2)
+            cv2.putText(right_annotated_frame, "Target", (cX - 30, cY + 20),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+            
+        if left_target_ID is not None and right_target_ID is not None:
+            matched_image, coordinate_matches = dc.left_right_match(undistorted_left, undistorted_right, 
+                                                                    left_target_bbox, right_target_bbox)
+            
+            if coordinate_matches is not None:
+                distance, x_coord = dc.find_distances(coordinate_matches, baseline, avg_focal_length)
+            
+        if left_annotated_frame is not None:
+            if move_state:
+                cv2.putText(left_annotated_frame, "GO", (new_width - 40, new_height - 20), 
+                            cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+            else:
+                cv2.putText(left_annotated_frame, "STOP", (new_width - 80, new_height - 20), 
+                            cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+                
+            cv2.putText(left_annotated_frame, f"{distance} cm", (10, new_height - 20), 
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+            cv2.circle(left_annotated_frame, (x_coord, int(new_height/2)), 4, (0, 0, 255), -1 )
+            cv2.imshow("Left", left_annotated_frame)
+
+        if right_annotated_frame is not None:
+            cv2.putText(right_annotated_frame, f"{distance} cm", (10, new_height - 20), 
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+            cv2.imshow("Right", right_annotated_frame)
+
+        if matched_image is not None: 
+            cv2.imshow("match image", matched_image)
+
+        new_L_pwm, new_R_pwm = controller.compute_speeds(distance, x_coord)
+        print(f"New Left PWM: {new_L_pwm},   New Right PWM: {new_R_pwm}")
 
         if not proximity_flag:
-            line.write_speeds(new_L_speed, new_R_speed)
+            line.write_speeds(int(new_L_pwm), int(new_R_pwm))
         else:
             line.write_speeds(0, 0)
-            
-        if left_detected_frame is not None :
-            cv2.imshow("Left", left_detected_frame)
-
-        if right_detected_frame is not None:
-            cv2.imshow("Right", right_detected_frame)
 
         if cv2.waitKey(1) == 27:
             break
 
     cv2.destroyAllWindows()
-    left.__del__()
-    right.__del__()
+    left_cam.__del__()
+    right_cam.__del__()
+
+if __name__ == "__main__":
+    main(2,0,640,480,"COM4",9600,1)
